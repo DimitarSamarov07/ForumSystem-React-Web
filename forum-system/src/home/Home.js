3b6e0227e41659dda1b9520003bc554bb872a6db
Dimitar Samarov <samarovdimitar@gmail.com>
2021-12-16 22:52:35 +0200

    Category collapse button now changes its text

--- forum-system/src/home/Home.js
+++ forum-system/src/home/Home.js
@@ -3,17 +3,23 @@ import LatestPosts from "./LatestPosts.js";
 import MostPopularPosts from "./MostPopularPosts.js";
 
 function Home() {
+    let changeCategoryCollapseText = () => {
+        let elem = document.getElementById("toggleCategoriesBtn");
+        if (elem.value === "Hide list of categories") elem.value = "Show list of categories";
+        else elem.value = "Hide list of categories";
+    }
+
     return (
 
         <div className="text-center">
             <h1 className="display-4">Welcome</h1>
 
             <br/>
-            {/*TODO: Add click functionality*/}
-            <input className="btn btn-outline-primary btn-block btn-lg" type="button"
+            <input onClick={changeCategoryCollapseText} className="btn btn-outline-primary btn-block btn-lg"
+                   type="button"
                    data-toggle="collapse"
                    data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
-                   value="Show list of categories" id="myButton1"/>
+                   value="Show list of categories" id="toggleCategoriesBtn"/>
 
             <div className="collapse" id="collapseExample">
                 <CategoriesCollapse/>
