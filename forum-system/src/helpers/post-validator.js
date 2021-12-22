393fa84202d339f561cc2a3145904c49977477c1
Dimitar Samarov <samarovdimitar@gmail.com>
2021-12-22 09:29:35 +0200

    Increase title length limit

--- forum-system/src/helpers/post-validator.js
+++ forum-system/src/helpers/post-validator.js
@@ -5,8 +5,8 @@ class PostValidator {
             return "Title is required.";
         } else if (value.length < 3) {
             return "Title should be at least 3 characters long."
-        } else if (value.length > 30) {
-            return "Title shouldn't exceed 20 characters"
+        } else if (value.length > 120) {
+            return "Title shouldn't exceed 120 characters"
         }
         return "";
     }
